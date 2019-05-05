aa1e13c374eeedc062c0dbe28f7dc98fd4a077a0
David Tsui <davidtsui@davids-air.home>
2019-05-05 18:36:48 +0100

    created game logic

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -1,7 +1,25 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+cards = ["queen","queen","king","king"]
+
+cardsInPlay = []
+
+var cardOne = cards[0]
+var cardTwo = cards[2]
+
+cardsInPlay.push('cardOne');
+cardsInPlay.push('cardTwo');
 
 console.log("User flipped "+ cardOne);
 console.log("User flipped "+ cardTwo);
+
+if (cardsInPlay.length === 2 && cardsInPlay[0]===cardsInPlay[2]){
+	alert("You found a match!");
+	
+} else alert("Sorry, try again.")
+
+
+// if (cardsInPlay[0]===cardsInPlay[2]){
+// 	alert("You found a match!");
+// } else alert("Sorry, try again.")
+
+// console.log("Two cards")
+// console.log("not enough cards in play")
